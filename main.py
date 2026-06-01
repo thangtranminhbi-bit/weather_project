c2099e835afce3c41e4124f73d3ceb2a781f2fb3
Thang <thangtranminhbi@gmail.com>
2026-06-02 00:05:14 +0700

    ghi file json

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import requests
 import os
 from datetime import date, timedelta
+import json
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="gcp_credentials.json"
 target_date = date.today() - timedelta(days=1)
 url = f'https://api.open-meteo.com/v1/forecast?latitude=10.823&longitude=106.6296&hourly=temperature_2m,relative_humidity_2m&start_date={target_date}&end_date={target_date}'
 res = requests.get(url)
 if res.status_code == 200:
-    print(res.json())
+    # Mở file để ghi (write) với định dạng utf-8 để hỗ trợ tiếng Việt
+    with open(f"ThoiTietNgay{target_date}.json", "w") as file:
+        json.dump(res.json(), file)
 else:
     print("API error: ", res.status_code)

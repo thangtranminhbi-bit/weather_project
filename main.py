3f1f622153874c84ccb63358a7a8f73719e52158
Thang <thangtranminhbi@gmail.com>
2026-06-15 14:02:21 +0700

    thu lai

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 from google.cloud import bigquery
 import pandas as pd
 from google.cloud import storage
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="gcp_credential.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="dbtWeather/Weather_dbt/gcp_credential.json"
 target_date = date.today() - timedelta(days=1)
 url = f'https://api.open-meteo.com/v1/forecast?latitude=10.823&longitude=106.6296&hourly=temperature_2m,relative_humidity_2m&start_date={target_date}&end_date={target_date}'
 res = requests.get(url)
